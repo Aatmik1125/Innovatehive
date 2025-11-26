93ca2456980279d453ab1447a5e79e7b0e4d5d65
Aatmik1125 <waghaatmik@gmail.com>
2025-11-26 21:04:46 +0530

    updated

--- app.py
+++ app.py
@@ -63,5 +63,7 @@ def contact():
     return render_template("contact.html")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
